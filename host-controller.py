f564ee3e3e60a0a9e752f059e683675f44219e0c
idk <patrickhingley@live.co.uk>
2025-09-11 14:40:22 +0100

    comments

--- host-controller.py
+++ host-controller.py
@@ -7,6 +7,7 @@ import datetime
 import sys
 import select
 
+# command line flags
 dummy = False
 reverse_arms = False
 time_dialation = 1 #Useful because sim doesn't run at 100% speed
@@ -23,13 +24,14 @@ np.set_printoptions(precision=3, suppress=True)
 
 print("Press ctrl+\ to quit process.")
 
+# neutral position
 idle_pos = np.asarray([0.0,0.0,1.5,0.15,0.5,0.225])
 
+# Go to set postitions for transition into/out of flat
 def goto_forward():
     arm0.labelRun("forward")
     if not dummy:
         arm1.labelRun("forward")
-
 def goto_flat():
     arm0.labelRun("startFlat")
     if not dummy:
@@ -41,15 +43,18 @@ ema_alpha = 0.1
 cartesian_cmds_moving_average = {True:np.zeros([7]), False:np.zeros([7])}
 
 def control_loop_single(arm, arm_angle, offset_from_accel, strength, flip_y, log=False):
+    # Determine goal position
     arm_influence = determine_arm_influence(arm_angle)
     target_pos = idle_pos + [0,0,0,0,-arm_influence*0.75,arm_influence]
     if flip_y:
         target_pos = target_pos * [-1,1,-1,1,-1,1]
     target_pos = target_pos + np.concatenate(([0,0,0], offset_from_accel))
 
+    # Determine unsmoothed velocity
     offset_from_target = target_pos - get_end_posture(arm)
     cartesian_cmd_instantaneous = np.concatenate((offset_from_target * strength, [0]))
 
+    # Calculate smoothed velocity and command arms
     cartesian_cmds_moving_average[flip_y] = \
         (cartesian_cmds_moving_average[flip_y] * (1-ema_alpha)) + \
         (cartesian_cmd_instantaneous * ema_alpha)
@@ -62,6 +67,7 @@ def control_loop_single(arm, arm_angle, offset_from_accel, strength, flip_y, log
 
 i = 0
 
+# Control both arms
 def control_loop_both(arm_angles, torso_accel, strength):
     global i
     log = i == 0
@@ -74,12 +80,14 @@ def control_loop_both(arm_angles, torso_accel, strength):
         print('')
     i = (i + 1)% 500
 
+# Calculate the given arms end effector position
 def get_end_posture(arm):
     return unitree_arm_interface.homoToPosture(
         arm._ctrlComp.armModel.forwardKinematics(
             arm.lowstate.getQ(), 6)
     )
 
+# Formula for angle -> displacement calculation
 def determine_arm_influence(angle):
     return -min(abs(angle) / 275, 0.3)
 
@@ -88,18 +96,16 @@ def determine_arm_influence(angle):
 ARM_1_UDP = unitree_arm_interface.UDPPort("127.0.0.1", 8073, 8074, unitree_arm_interface.RECVSTATE_LENGTH, unitree_arm_interface.BlockYN.NO, 500000)
 arm1 =  unitree_arm_interface.ArmInterface()
 arm1._ctrlComp.udp = ARM_1_UDP
-
 arm0 = unitree_arm_interface.ArmInterface()
 
+# Activate both arms
 arm1.loopOn()
 arm0.loopOn()
 
+# If we are not in DUMMY mode, activate serial
 ser = None
-
-# serial config
 if not dummy:
     ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=0, rtscts=True)
-
 serial_line_buffer = b''
 most_recent_serial_line = "0, 0, 0, 0, 0"
 most_recent_serial_time = datetime.datetime(year=1970,month=1,day=1)
@@ -158,7 +164,7 @@ while True:
                         mode = "idle"
                     most_recent_serial_time = datetime.datetime.now()
 
-        # Apply per cycle instructions if needed:
+        # Apply per cycle instructions if needed (run, idle):
         if mode == "run":
             if \
                 (datetime.datetime.now() - most_recent_serial_time) > datetime.timedelta(seconds=0.1) \
@@ -180,5 +186,6 @@ while True:
             control_loop_both([0, 0], [0, 0, -1], 2.5)
         time.sleep(arm0._ctrlComp.dt / time_dialation)
     except Exception as e:
+        # Handle exceptions and continue control loop in idle
         print("Returning to idle due to exception:", e)
         mode = "idle"
